ce4eb2e4c05bebf44e243803ba18eb461a182131
hazzard Maddison <willhettichpo@gmail.com>
2022-08-26 01:06:17 -0500

    Chapter 19 : Vertex Buffer Layouts

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -8,6 +8,26 @@ namespace Hazel {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case ShaderDataType::Float:  return GL_FLOAT;
+			case ShaderDataType::Float2: return GL_FLOAT;
+			case ShaderDataType::Float3: return GL_FLOAT;
+			case ShaderDataType::Float4: return GL_FLOAT;
+			case ShaderDataType::Mat3:   return GL_FLOAT;
+			case ShaderDataType::Mat4:   return GL_FLOAT;
+			case ShaderDataType::Int:    return GL_INT;
+			case ShaderDataType::Int2:   return GL_INT;
+			case ShaderDataType::Int3:   return GL_INT;
+			case ShaderDataType::Int4:   return GL_INT;
+			case ShaderDataType::Bool:   return GL_BOOL;
+		}
+		HZ_CORE_ASSERT(false, "Unknown shader data type");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		HZ_CORE_ASSERT(!s_Instance, "Application already exists");
@@ -21,16 +41,36 @@ namespace Hazel {
 
 
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[7 * 3] = {
+			-0.5f, -0.5f, 0.0f,    1.0f, 1.0f, 0.0f, 1.0f,
+			 0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 1.0f, 1.0f,
+			 0.0f,  0.5f, 0.0f,    1.0f, 0.0f, 1.0f, 1.0f,
 		};
 
 		m_VertexBuffer = std::unique_ptr<VertexBuffer>(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position"},
+				{ ShaderDataType::Float4, "a_Color"}
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		const auto& layout = m_VertexBuffer->GetLayout();
+		uint32_t index = 0;
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+								  element.GetComponentCount(), 
+								  ShaderDataTypeToOpenGLBaseType(element.Type), 
+								  element.Normalized ? GL_TRUE : GL_FALSE, 
+								  layout.GetStride(), 
+								  (const void*)element.Offset);
+			++index;
+		}
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		m_IndexBuffer = std::unique_ptr<IndexBuffer>(IndexBuffer::Create(indices,sizeof(indices) / sizeof(uint32_t)));
@@ -39,12 +79,15 @@ namespace Hazel {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
@@ -55,10 +98,12 @@ namespace Hazel {
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5 , 1.0);
+				color = v_Color;
 			}
 		)";
 
