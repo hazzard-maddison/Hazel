a323bb9ebc30da5e99a5f21870ca0dd4942153db
hazzard Maddison <willhettichpo@gmail.com>
2022-09-10 13:32:52 -0500

    Chapter 24 : Material Systems

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -7,7 +7,7 @@ class ExampleLayer : public Hazel::Layer{
 
 public:
 	ExampleLayer() :
-			Layer("Example Layer"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_SquarePosition(0.0f)
+			Layer("Example Layer"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f)
 		{
 
 			m_CameraPosition = m_Camera.GetPosition();
@@ -76,10 +76,10 @@ public:
 			m_SquareVA.reset(Hazel::VertexArray::Create());
 
 			float squareVertices[4 * 3] = {
-				-0.75f, -0.75f, 0.0f,
-				 0.75f, -0.75f, 0.0f,
-				 0.75f,  0.75f, 0.0f,
-				-0.75f,  0.75f, 0.0f
+				-0.5f, -0.5f, 0.0f,
+				 0.5f, -0.5f, 0.0f,
+				 0.5f,  0.5f, 0.0f,
+				-0.5f,  0.5f, 0.0f
 			};
 
 			std::shared_ptr<Hazel::VertexBuffer> squareVB;
@@ -96,7 +96,7 @@ public:
 			m_SquareVA->SetIndexBuffer(squareIB);
 
 			// Square
-			std::string blueShaderVertexSrc = R"(
+			std::string flatColorShaderVertexSrc = R"(
 				#version 330 core
 			
 				layout(location = 0) in vec3 a_Position;
@@ -113,20 +113,22 @@ public:
 				}
 			)";
 
-			std::string blueShaderFragmentSrc = R"(
+			std::string flatColorShaderFragmentSrc = R"(
 				#version 330 core
 			
 				layout(location = 0) out vec4 color;
 
 				in vec3 v_Position; 
 
+				uniform vec4 u_Color;
+
 				void main()
 				{
-					color = vec4(0.5f * v_Position.y, 0.05f, v_Position.y * 0.5f + 0.5, 1.0f);
+					color = u_Color;
 				}
 			)";
 
-			m_BlueShader.reset(new Hazel::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+			m_FlatColorShader.reset(new Hazel::Shader(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 
 		}
 
@@ -161,24 +163,7 @@ public:
 			m_CameraRotation -= m_CameraRotationSpeed * timestep;
 		}
 
-		// Square Move
-		if (Hazel::Input::IsKeyPressed(HZ_KEY_J))
-		{
-			m_SquarePosition.x -= m_SquareMoveSpeed * timestep;
-		}
-		else if (Hazel::Input::IsKeyPressed(HZ_KEY_L))
-		{
-			m_SquarePosition.x += m_SquareMoveSpeed * timestep;
-		}
-
-		if (Hazel::Input::IsKeyPressed(HZ_KEY_I))
-		{
-			m_SquarePosition.y += m_SquareMoveSpeed * timestep;
-		}
-		else if (Hazel::Input::IsKeyPressed(HZ_KEY_K))
-		{
-			m_SquarePosition.y -= m_SquareMoveSpeed * timestep;
-		}
+		
 
 
 		Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
@@ -189,10 +174,29 @@ public:
 
 
 		Hazel::Renderer::BeginScene(m_Camera);
-		
-		glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_SquarePosition);
 
-		Hazel::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+		
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
+		glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				
+				if (x % 2 == 0)
+					m_FlatColorShader->UploadUniformFloat4("u_Color", redColor);
+				else
+					m_FlatColorShader->UploadUniformFloat4("u_Color", blueColor);
+
+				Hazel::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
+			}
+		}
+		
 
 		Hazel::Renderer::Submit(m_Shader, m_VertexArray);
 
@@ -213,7 +217,7 @@ private:
 	std::shared_ptr<Hazel::VertexArray> m_VertexArray;
 
 
-	std::shared_ptr<Hazel::Shader> m_BlueShader;
+	std::shared_ptr<Hazel::Shader> m_FlatColorShader;
 	std::shared_ptr<Hazel::VertexArray> m_SquareVA;
 
 	Hazel::OrthographicCamera m_Camera;
@@ -223,8 +227,7 @@ private:
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 45.0f;
 
-	glm::vec3 m_SquarePosition;
-	float m_SquareMoveSpeed = 1.0f;
+	
 
 };
 
